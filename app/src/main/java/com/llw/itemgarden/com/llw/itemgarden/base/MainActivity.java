c62e3eff497e3ccb2c565a770e76764d42ece6ff
Administrator <994536798@qq.com>
2015-03-13 09:59:26 +0800

    create some methods

---
+++ app/src/main/java/com/llw/itemgarden/com/llw/itemgarden/base/MainActivity.java
@@ -0,0 +1,106 @@
+package com.llw.itemgarden.com.llw.itemgarden.base;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+
+import com.llw.itemgarden.R;
+
+
+public class MainActivity extends FragmentActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addFragment(null,true);
+    }
+
+    /**
+     * add a Fragment
+     */
+    public void addFragment(Class <? extends Fragment> fragmentClass, boolean isAddToBackStack){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        Fragment fragment = null;
+        try{
+            fragment = fragmentClass.newInstance();
+        }catch (InstantiationException e){
+            e.printStackTrace();
+        }catch (IllegalAccessException e){
+            e.printStackTrace();
+        }
+        String fragmentName = fragmentClass.getSimpleName();
+        transaction.add(getFragmentContainerId(), fragment, fragmentName);
+        if(isAddToBackStack)
+            transaction.addToBackStack(fragmentName);
+        transaction.commitAllowingStateLoss();
+    }
+
+    /**
+     * replace a Fragment
+     */
+    public void replaceFragment(Class<? extends  Fragment> fragmentClass, boolean isAddToBackStack){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        Fragment fragment = null;
+        try{
+            fragment = fragmentClass.newInstance();
+        }catch (InstantiationException e){
+            e.printStackTrace();
+        }catch (IllegalAccessException e){
+            e.printStackTrace();
+        }
+        String fragmentName = fragmentClass.getSimpleName();
+        transaction.replace(getFragmentContainerId(), fragment, fragmentName);
+        if(isAddToBackStack)
+            transaction.addToBackStack(fragmentName);
+        transaction.commitAllowingStateLoss();
+    }
+
+    /**
+     * show a Fragment
+     */
+    public void showFragment(Class<? extends Fragment> fragmentClass){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        Fragment fragment = fragmentManager.findFragmentByTag(fragmentClass.getSimpleName());
+        if(fragment == null){
+            try {
+                fragment = fragmentClass.newInstance();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        transaction.show(fragment);
+        transaction.commitAllowingStateLoss();
+    }
+
+    /**
+     * hide a Fragment
+     */
+    public void hideFragment(Class<? extends Fragment> fragmentClass){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        Fragment fragment = fragmentManager.findFragmentByTag(fragmentClass.getSimpleName());
+        if(fragment == null){
+            try {
+                fragment = fragmentClass.newInstance();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        transaction.hide(fragment);
+        transaction.commitAllowingStateLoss();
+    }
+
+    private int getFragmentContainerId(){
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/llw/itemgarden/utils/CrashHandler.java
@@ -0,0 +1,50 @@
+package com.llw.itemgarden.utils;
+
+import android.content.Context;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2015/3/12.
+ */
+public class CrashHandler implements Thread.UncaughtExceptionHandler{
+    private static CrashHandler mCrashHandler;
+    private Thread.UncaughtExceptionHandler mDefaultHandler;
+    private Context mContext;
+    private CrashHandler(){
+    }
+
+    public static CrashHandler getInstance(){
+        if(mCrashHandler == null)
+            mCrashHandler = new CrashHandler();
+        return mCrashHandler;
+    }
+
+    public void initCrashHandler(Context context){
+        this.mContext = context;
+        mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(this);
+    }
+    @Override
+    public void uncaughtException(Thread thread, Throwable ex) {
+        try {
+            Log.e("uncaugthException", "uncaugthException", ex);
+            handleCrashException(ex);
+        } catch (Exception e) {
+            if(mDefaultHandler != null)
+                mDefaultHandler.uncaughtException(thread, ex);
+        }
+
+    }
+
+    /**
+     * handle exception, make a toast to users or do other things
+     */
+    private void handleCrashException(Throwable throwable){
+        String message = throwable.getLocalizedMessage();
+        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+        Looper.prepare();
+        Looper.loop();
+    }
+}
